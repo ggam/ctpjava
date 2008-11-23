b32e783ed5cad493d557c0d77195725f29ef4450
thomas.hug <thomas.hug@c87da586-b896-11dd-8af8-77a15857c330>
2008-11-23 21:52:33 +0000

    Smarter hot deploy directory clean

--- trunk/projects/maven-hotdeploy-plugin/src/main/java/com/ctp/seam/maven/HotDeployMojo.java
+++ trunk/projects/maven-hotdeploy-plugin/src/main/java/com/ctp/seam/maven/HotDeployMojo.java
@@ -5,7 +5,9 @@ package com.ctp.seam.maven;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.maven.plugin.CompilationFailureException;
 import org.apache.maven.plugin.CompilerMojo;
@@ -17,7 +19,7 @@ import org.apache.maven.plugin.MojoExecutionException;
  *
  */
 public class HotDeployMojo extends CompilerMojo {
-    
+
     /**
      * Where to look for the hot deployable sources.
      */
@@ -72,31 +74,70 @@ public class HotDeployMojo extends CompilerMojo {
         File dir = new File(deployDirectory + File.separator + deployPath);
         getLog().debug("Cleaning " + dir.getAbsolutePath());
         if (dir.exists() && dir.isDirectory()) {
-            getLog().debug("Running delete on dir " + dir.getAbsolutePath());
-            delete(dir, true);
+            getLog().debug("Running clean on dir " + dir.getAbsolutePath());
+            synchFolders(getOutputDirectory(), dir);
+        }
+    }
+    
+    /**
+     * Synchronize the content of two folders. Sync rules are:
+     * - If the target content is not present in the source content, delete it
+     * - If the target content file is older than the source file, delete it.
+     * @param source            Source folder - not changed.
+     * @param target            Target folder - changed according to sync rules.
+     */
+    private void synchFolders(File source, File target) {
+        File[] deployed = target.listFiles();
+        Map sourceContent = contentAsMap(source);
+        for (int i = 0; i < deployed.length; i++) {
+            String key = deployed[i].getName();
+            if (!sourceContent.keySet().contains(key)) {
+                delete(deployed[i]);
+                return;
+            }
+            File compiled = (File) sourceContent.get(key);
+            if (deployed[i].isFile() && compiled.lastModified() > deployed[i].lastModified()) {
+                delete(deployed[i]);
+                return;
+            }
+            if (compiled.isDirectory() && deployed[i].isDirectory()) {
+                synchFolders(compiled, deployed[i]);
+            }
         }
     }
     
     /**
      * Recursively delete a file tree.
      * @param file              File to be deleted, including the children.
-     * @param skipFirst         The deploy folder should not be deleted.
      * @return                  {@code true} for success.
      */
-    private boolean delete(File file, boolean skipFirst) {
+    private boolean delete(File file) {
         if (file.isDirectory()) {
             File[] children = file.listFiles();
             for (int i = 0; i < children.length; i++) {
-                boolean couldDelete = delete(children[i], false);
+                boolean couldDelete = delete(children[i]);
                 if (!couldDelete) {
                     getLog().warn("File " + children[i].getName() + " could not be deleted.");
                 }
             }
         }
-        if (skipFirst)
-            return true;
         getLog().debug("Delete file " + file.getAbsolutePath());
         return file.delete();
+    }
+    
+    /**
+     * Helper method: Extract a content map from a directory with
+     * the file name as key and the file itself as value.
+     * @param dir               Directory to extract content.
+     * @return                  Map instance.
+     */
+    private Map contentAsMap(File dir) {
+        File[] content = dir.listFiles();
+        Map result = new HashMap();
+        for (int i = 0; i < content.length; i++) {
+            result.put(content[i].getName(), content[i]);
+        }
+        return result;
     }
 
 }
