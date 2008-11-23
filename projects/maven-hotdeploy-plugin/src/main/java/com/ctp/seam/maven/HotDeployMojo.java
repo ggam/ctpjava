e04c306c323f4f55fe74896f33e30df7db7785b6
thomas.hug <thomas.hug@c87da586-b896-11dd-8af8-77a15857c330>
2008-11-23 09:52:40 +0000

    Added hot deploy class folder clean.

--- projects/maven-hotdeploy-plugin/src/main/java/com/ctp/seam/maven/HotDeployMojo.java
+++ projects/maven-hotdeploy-plugin/src/main/java/com/ctp/seam/maven/HotDeployMojo.java
@@ -7,7 +7,9 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.maven.plugin.CompilationFailureException;
 import org.apache.maven.plugin.CompilerMojo;
+import org.apache.maven.plugin.MojoExecutionException;
 
 /**
  * Adapt the default compiler mojo to be used for Seam hot deployment.
@@ -22,9 +24,28 @@ public class HotDeployMojo extends CompilerMojo {
     private String sourceDirectory;
     
     /**
+     * The directory the application gets deployed in. Not the app server directory,
+     * directly the application directory containing the /WEB-INF directory.
+     */
+    private String deployDirectory;
+    
+    /**
      * Where to deploy the compiled sources to in the WAR file. 
      */
     private String deployPath;
+    
+
+    /* (non-Javadoc)
+     * @see org.apache.maven.plugin.CompilerMojo#execute()
+     */
+    public void execute() throws MojoExecutionException,
+            CompilationFailureException {
+        if (deployDirectory == null) {
+            throw new MojoExecutionException("deployDirectory must be set.");
+        }
+        super.execute();
+        cleanDeployDirectory();
+    }
 
     /* (non-Javadoc)
      * @see org.apache.maven.plugin.CompilerMojo#getCompileSourceRoots()
@@ -41,6 +62,41 @@ public class HotDeployMojo extends CompilerMojo {
     protected File getOutputDirectory() {
         File configured = super.getOutputDirectory();
         return new File(configured.getAbsolutePath() + deployPath);
+    }
+
+    
+    /**
+     * Old hot deployable files need to be cleaned for the changes to be picked up.
+     */
+    protected void cleanDeployDirectory() {
+        File dir = new File(deployDirectory + File.separator + deployPath);
+        getLog().debug("Cleaning " + dir.getAbsolutePath());
+        if (dir.exists() && dir.isDirectory()) {
+            getLog().debug("Running delete on dir " + dir.getAbsolutePath());
+            delete(dir, true);
+        }
+    }
+    
+    /**
+     * Recursively delete a file tree.
+     * @param file              File to be deleted, including the children.
+     * @param skipFirst         The deploy folder should not be deleted.
+     * @return                  {@code true} for success.
+     */
+    private boolean delete(File file, boolean skipFirst) {
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            for (int i = 0; i < children.length; i++) {
+                boolean couldDelete = delete(children[i], false);
+                if (!couldDelete) {
+                    getLog().warn("File " + children[i].getName() + " could not be deleted.");
+                }
+            }
+        }
+        if (skipFirst)
+            return true;
+        getLog().debug("Delete file " + file.getAbsolutePath());
+        return file.delete();
     }
 
 }
