3906f7e2a773291061b1612cab5c6ae0694701f1
bartosz.majsak@gmail.com <bartosz.majsak@gmail.com@c87da586-b896-11dd-8af8-77a15857c330>
2010-08-12 08:34:59 +0000

    JPA2 metamodel

---
+++ trunk/arquillian-demo-02/target/metamodel/com/ctp/arquilliandemo/ex2/domain/TradeTransaction_.java
@@ -0,0 +1,18 @@
+package com.ctp.arquilliandemo.ex2.domain;
+
+import java.util.Date;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@StaticMetamodel(TradeTransaction.class)
+public abstract class TradeTransaction_ {
+
+	public static volatile SingularAttribute<TradeTransaction, Integer> amount;
+	public static volatile SingularAttribute<TradeTransaction, Date> timestamp;
+	public static volatile SingularAttribute<TradeTransaction, Long> id;
+	public static volatile SingularAttribute<TradeTransaction, Share> share;
+	public static volatile SingularAttribute<TradeTransaction, TransactionType> type;
+	public static volatile SingularAttribute<TradeTransaction, User> user;
+
+}
+
