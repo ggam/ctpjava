903078b3eefadfea0bd105232a5fb02de8d476d1
bartosz.majsak@gmail.com <bartosz.majsak@gmail.com@c87da586-b896-11dd-8af8-77a15857c330>
2010-08-12 08:57:04 +0000

    Note about seam3 examples

--- trunk/arquillian-demo-02/src/test/java/com/ctp/test/cdi/scope/HashMapBeanStore.java
+++ trunk/arquillian-demo-02/src/test/java/com/ctp/test/cdi/scope/HashMapBeanStore.java
@@ -7,6 +7,11 @@ import java.util.Map;
 import org.jboss.weld.context.api.ContextualInstance;
 import org.jboss.weld.context.api.helpers.AbstractMapBackedBeanStore;
 
+/**
+ * 
+ * Taken from Seam 3 examples : http://anonsvn.jboss.org/repos/seam/examples/trunk/
+ *
+ */
 public class HashMapBeanStore extends AbstractMapBackedBeanStore implements Serializable {
 
     private static final long serialVersionUID = 956315549655640869L;
